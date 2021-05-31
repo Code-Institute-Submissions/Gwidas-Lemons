eed81f85c523b0d8e3db37cca35eda6ca9160083
Gwidas <gwidas.hiroshi@icloud.com>
2021-05-31 11:50:35 +0000

    added calculator function to nutrition.js, updated nutrition.html with grid system

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,5 +1,5 @@
  
- var wateronaliter = 800;
+ /*var wateronaliter = 800;
  var lemonsonaliter = 13;
  var sugaronaliter = 8;
  
@@ -11,4 +11,35 @@
         document.getElementById('water').innerHTML = waterneeded;
         document.getElementById('sugar').innerHTML = sugarneeded;
         document.getElementById('lemons').innerHTML = lemonsneeded;
- }
+ }*/
+
+ 
+const buttons = document.querySelectorAll('button');
+
+const display = document.querySelector('.display');
+
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+
+function calculate(event) {
+  
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    
+    if (display.value !== '') {
+      
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    
+    display.value = '';
+  } else {
+    
+    display.value += clickedButtonValue;
+  }
+}
+
