e46f72dc14ab15b2396f0ad80726a6cee612fe9d
Gwidas <gwidas.hiroshi@icloud.com>
2021-06-01 16:32:58 +0000

    confihuring nutrition.js

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,18 +1,41 @@
  
- /*var wateronaliter = 800;
+ var wateronaliter = 800;
  var lemonsonaliter = 13;
- var sugaronaliter = 8;
  
- function lemonade (){
-        var liters = parseInt(document.getElementById('liters'));
-        var lemonsneeded = lemonsonaliter * liters
-        var waterneeded = wateronaliter * liters
-        var sugarneeded = sugaronaliter * liters 
+ 
+ function lemonade (sweetness){
+        
+        if (sweetness == "Mild sweet"){
+          var sugaronaliter = 8;
+          console.log(sugaronaliter)
+        }
+        if (sweetness == "Medium sweet") {
+          var sugaronaliter = 12; 
+          console.log(sugaronaliter)
+        }
+        if (sweetness == "Very sweet"){
+          var sugaronaliter = 16;
+          console.log(sugaronaliter)
+
+        }
+        var liters = document.getElementById('liters').innerHTML;
+        console.log("liters: {0}", liters);
+        console.log("before calculation lemons: " + lemonsonaliter);
+        console.log("before calculation water: " + wateronaliter);
+        console.log("before calculation sugar: " + sugaronaliter);
+        var lemonsneeded = (lemonsonaliter * liters);
+        var waterneeded = (wateronaliter * liters);
+        var sugarneeded =  sugaronaliter * liters; 
+        console.log("after calculation lemons: " + lemonsneeded);
+        console.log("after calculation water: " + waterneeded);
+        console.log("after calculation sugar: " + sugarneeded);
         document.getElementById('water').innerHTML = waterneeded;
         document.getElementById('sugar').innerHTML = sugarneeded;
         document.getElementById('lemons').innerHTML = lemonsneeded;
- }*/
-function lemonacalulator (){
+ }
+
+ 
+//function lemonadeCalculator (){
 // get inputs
 
 // mathemetical fucntion 
@@ -20,34 +43,7 @@ function lemonacalulator (){
 // updated the element 
 
 
-}
- 
-const buttons = document.querySelectorAll('button');
-
-const display = document.querySelector('.display');
 
 
-buttons.forEach(function(button) {
-  button.addEventListener('click', calculate);
-});
 
-
-function calculate(event) {
-  
-  const clickedButtonValue = event.target.value;
-
-  if (clickedButtonValue === '=') {
-    
-    if (display.value !== '') {
-      
-      display.value = eval(display.value);
-    }
-  } else if (clickedButtonValue === 'C') {
-    
-    display.value = '';
-  } else {
-    
-    display.value += clickedButtonValue;
-  }
-}
 
