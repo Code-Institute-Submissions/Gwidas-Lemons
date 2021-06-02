142ecce3ded9e00a06597ca42e69b345016e89d9
Gwidas <gwidas.hiroshi@icloud.com>
2021-06-02 12:43:22 +0000

    removed elemtn button

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,41 +1,6 @@
- 
- /*var wateronaliter = 0.8;
- var lemonsonaliter = 13;
- 
- 
- function lemonade (sweetness){
-        
-        if (sweetness == "Mild sweet"){
-          var sugaronaliter = 8;
-          console.log(sugaronaliter)
-        }
-        if (sweetness == "Medium sweet") {
-          var sugaronaliter = 12; 
-          console.log(sugaronaliter)
-        }
-        if (sweetness == "Very sweet"){
-          var sugaronaliter = 16;
-          console.log(sugaronaliter)
 
-        }
-        var liters = document.getElementById('liters').value;
-        var lemonsneeded = (lemonsonaliter * liters);
-        var waterneeded = (wateronaliter * liters);
-        var sugarneeded =  sugaronaliter * liters; 
-        document.getElementById('water').innerHTML = waterneeded;
-        document.getElementById('sugar').innerHTML = sugarneeded;
-        document.getElementById('lemons').innerHTML = lemonsneeded;
- }*/
 
- 
-//function lemonadeCalculator (){
-// get inputs
-
-// mathemetical fucntion 
-
-// updated the element 
-
-// Recipe calculator with jquery
+// Recipe calculator 
 var computeServing = function(serving) {
   $('.js-recipeIngredient').each(function(index, item) {
     $(item).children('span').html($(item)[0].dataset.basevalue * serving)
@@ -57,7 +22,7 @@ $('.js-increaseService').on('click', function() {
 computeServing(1);
 
 
-
+      /*steps*/
 $( function() {
   $( "#lemon" ).accordion();
 } );
