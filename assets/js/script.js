45092a0363ebee841f9163ade516cd6811d2b16d
Gwidas <gwidas.hiroshi@icloud.com>
2022-02-01 13:14:21 +0000

    Map allocation in lacation.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*------- Locaton Map --------*/
+
 var map = L.map('map').setView([0, 0], 1);
 
       L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=JirbcBhhMNnI5MS919bO', {
