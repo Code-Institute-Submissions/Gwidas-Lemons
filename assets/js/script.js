8c4b725bb4babcac8d9f2a2867360435763e3815
Gwidas <gwidas.hiroshi@icloud.com>
2022-02-03 13:58:50 +0000

    Added font to navbar, created card and descritopn of nutition.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/*------- Locaton Map --------*/
+/*------- Location Map --------*/
 
 var map = L.map('map').setView([0, 0], 1);
 
@@ -31,63 +31,5 @@ var map = L.map('map').setView([0, 0], 1);
       
       map.on('click', onMapClick);
 
-
-/*Calculator 
-
-let display = document.getElementById('display');
-
-let buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error";
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
-}); */
-
-
-
-
-
-
-
-
-/* Recipe calculator 
-var computeServing = function(serving) {
-  $('.js-recipeIngredient').each(function(index, item) {
-    $(item).children('span').html($(item)[0].dataset.basevalue * serving)
-  })
-}
-$('#servingInput').on('change', function() {
-  computeServing($(this).val())
-})
-$('.js-decreaseService').on('click', function() {
-  var currentServing = $('#servingInput').val()
-  $('#servingInput').val(currentServing - 1)
-  computeServing(currentServing - 1)
-})
-$('.js-increaseService').on('click', function() {
-  var currentServing = $('#servingInput').val()
-  $('#servingInput').val(parseInt(currentServing) + 1)
-  computeServing(parseInt(currentServing) + 1)
-})
-computeServing(1)*/
 
 
